4874dd68af72beacc65e51a75ad5088a9529b259
at15 <at15@outlook.com>
2015-12-26 22:03:49 +0800

    Add test for put

--- indexer/src/test/java/io/dongyue/at15/tree/indexer/bptree/MellowtechBtreeTest.java
+++ indexer/src/test/java/io/dongyue/at15/tree/indexer/bptree/MellowtechBtreeTest.java
@@ -1,6 +1,7 @@
 package io.dongyue.at15.tree.indexer.bptree;
 
 import io.dongyue.at15.tree.indexer.bptree.mellowtech.MellowBPTree;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,21 +9,40 @@ import org.junit.Test;
  * Created by at15 on 15-12-26.
  */
 public class MellowtechBtreeTest {
+    private String path = "/tmp/mellowbptreeindex";
+
     @Test
     public void testOpen() {
         BPTreeIndex bpTreeIndex = new MellowBPTree();
-        Assert.assertEquals(true, bpTreeIndex.open("/tmp/mellowbptreeindex"));
+        Assert.assertEquals(true, bpTreeIndex.open(path));
     }
 
     @Test
     public void testCreate() {
         BPTreeIndex bpTreeIndex = new MellowBPTree();
-        Assert.assertEquals(true, bpTreeIndex.create("/tmp/mellowbptreeindex"));
+        Assert.assertEquals(true, bpTreeIndex.create(path));
     }
 
     @Test
-    public void testRemove(){
+    public void testPut() {
         BPTreeIndex bpTreeIndex = new MellowBPTree();
-        bpTreeIndex.removeIfExists("/tmp/mellowbptreeindex");
+        bpTreeIndex.create(path);
+        bpTreeIndex.put(1, "jack");
+        Assert.assertEquals("jack", bpTreeIndex.get(1));
     }
+
+    @Test
+    public void testRemove() {
+        BPTreeIndex bpTreeIndex = new MellowBPTree();
+        bpTreeIndex.removeIfExists(path);
+    }
+
+    @After
+    public void cleanUp(){
+        // in case you just run one test in IDEA
+        BPTreeIndex bpTreeIndex = new MellowBPTree();
+        bpTreeIndex.removeIfExists(path);
+    }
+
+
 }
