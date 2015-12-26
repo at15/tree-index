95e36c4fc74394c9de5f9fd6c5c23425a23df248
at15 <at15@outlook.com>
2015-12-26 16:36:50 +0800

    Add shell class

---
+++ shell/src/main/java/io/dongyue/at15/tree/shell/Shell.java
@@ -0,0 +1,15 @@
+package io.dongyue.at15.tree.shell;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by at15 on 15-12-26.
+ */
+public class Shell {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Shell.class);
+
+    public static void main(String[] args) {
+        // TODO: parse args
+    }
+}
