62c0143877349c770ad1ea95af416613bfc65613
at15 <at15@outlook.com>
2015-12-26 20:31:30 +0800

    Add test

---
+++ shell/src/test/java/io/dongyue/at15/tree/shell/ShellTest.java
@@ -0,0 +1,14 @@
+package io.dongyue.at15.tree.shell;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by at15 on 15-12-26.
+ */
+public class ShellTest {
+    @Test
+    public void testDummy(){
+        Assert.assertEquals("dummy","dummy");
+    }
+}
