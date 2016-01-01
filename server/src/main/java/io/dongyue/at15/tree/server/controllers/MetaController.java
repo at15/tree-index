7ea1eace10f0b3799050adc5ab097083c5a86f05
at15 <at15@outlook.com>
2016-01-02 03:15:58 +0800

    using raw hdfs is working ....

--- server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
+++ server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.hadoop.fs.FsShell;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 /**
  * Created by at15 on 16-1-2.
@@ -14,15 +20,21 @@ import org.springframework.data.hadoop.fs.FsShell;
 @RestController
 //@RequestMapping("/meta")
 public class MetaController {
-    @Autowired
-    private FsShell shell;
+    protected Configuration conf;
+    protected FileSystem hdfs;
 
     //    @RequestMapping("/")
     @RequestMapping("/meta")
-    public String index() {
-        // TODO: should list all meta
-        shell.ls("/user");
-        return "please specify which meta you need";
+    public String index() throws IOException {
+        // use raw hdfs
+        conf = new Configuration();
+        try {
+            hdfs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
+            return hdfs.getHomeDirectory().toString();
+        } catch (URISyntaxException ex) {
+            throw new IOException(ex);
+        }
+//        return "please specify which meta you need";
     }
 
     @RequestMapping("/{table}/meta")
