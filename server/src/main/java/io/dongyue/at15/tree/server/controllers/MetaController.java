424500bf3ff9281c42b82384eeca942f1903fa7c
at15 <at15@outlook.com>
2016-01-02 02:36:43 +0800

    hdfs connection failed ....

--- server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
+++ server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
@@ -1,8 +1,12 @@
 package io.dongyue.at15.tree.server.controllers;
 
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.hadoop.fs.FsShell;
 
 /**
  * Created by at15 on 16-1-2.
@@ -10,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@RequestMapping("/meta")
 public class MetaController {
+    @Autowired
+    private FsShell shell;
+
     //    @RequestMapping("/")
     @RequestMapping("/meta")
     public String index() {
         // TODO: should list all meta
+        shell.ls("/user");
         return "please specify which meta you need";
     }
 
