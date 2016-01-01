f5fc1fd7b901917932b34ebd6998a5e186ebbb92
at15 <at15@outlook.com>
2016-01-02 01:45:28 +0800

    Index controller is working

---
+++ server/src/main/java/io/dongyue/at15/tree/server/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package io.dongyue.at15.tree.server.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Created by at15 on 16-1-1.
+ * <p/>
+ * Index
+ */
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hi, you got me";
+    }
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
