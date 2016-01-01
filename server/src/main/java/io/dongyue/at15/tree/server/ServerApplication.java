065315204e65e7f8bac26ce851f9f8a87c5e1f02
at15 <at15@outlook.com>
2016-01-02 03:08:15 +0800

    Can't config spring hadoop

--- server/src/main/java/io/dongyue/at15/tree/server/ServerApplication.java
+++ server/src/main/java/io/dongyue/at15/tree/server/ServerApplication.java
@@ -3,7 +3,12 @@ package io.dongyue.at15.tree.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class ServerApplication {
 
 	public static void main(String[] args) {
