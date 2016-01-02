6f42306e5a5680f7447dc9b5ce8ab936c36b77c4
at15 <at15@outlook.com>
2016-01-03 02:23:47 +0800

    Add getRow and load from file for MetaTable

--- common/src/main/java/io/dongyue/at15/tree/common/format/MetaTable.java
+++ common/src/main/java/io/dongyue/at15/tree/common/format/MetaTable.java
@@ -1,7 +1,39 @@
 package io.dongyue.at15.tree.common.format;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Created by at15 on 16-1-2.
  */
 public class MetaTable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetaTable.class);
+    private ArrayList<Meta> rows;
+
+    public MetaTable() {
+        rows = new ArrayList<Meta>();
+    }
+
+    public MetaTable(File file) throws IOException {
+        this();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = bufferedReader.readLine();
+//            LOGGER.debug(line);
+            while (line != null) {
+                rows.add(Meta.parse(line));
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            throw new IOException("meta table file not found", ex);
+        }
+    }
+
+    // TODO: should throw some exception
+    public Meta getRow(Integer row) {
+        return rows.get(row);
+    }
 }
