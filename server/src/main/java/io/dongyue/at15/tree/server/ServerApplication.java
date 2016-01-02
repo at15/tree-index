dcd9718601f91e3eb06101f05c3e65076a581cd6
at15 <at15@outlook.com>
2016-01-02 12:29:09 +0800

    somehow the spring hadoop start working XD

--- server/src/main/java/io/dongyue/at15/tree/server/ServerApplication.java
+++ server/src/main/java/io/dongyue/at15/tree/server/ServerApplication.java
@@ -1,12 +1,24 @@
 package io.dongyue.at15.tree.server;
 
+import org.apache.hadoop.fs.FileStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.hadoop.fs.FsShell;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication implements CommandLineRunner {
+	@Autowired
+	private FsShell shell;
+
+    public void run(String... args) {
+        for (FileStatus s : shell.lsr("/tmp")) {
+            System.out.println("> " + s.getPath());
+        }
+    }
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
 	}
 }
--- server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
+++ server/src/main/java/io/dongyue/at15/tree/server/controllers/MetaController.java
@@ -1,6 +1,9 @@
 package io.dongyue.at15.tree.server.controllers;
 
 
+import org.apache.hadoop.fs.FileStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.hadoop.fs.FsShell;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,23 +21,16 @@ import java.net.URISyntaxException;
  * Created by at15 on 16-1-2.
  */
 @RestController
-//@RequestMapping("/meta")
 public class MetaController {
-    protected Configuration conf;
-    protected FileSystem hdfs;
+    @Autowired
+    private FsShell shell;
 
-    //    @RequestMapping("/")
     @RequestMapping("/meta")
     public String index() throws IOException {
-        // use raw hdfs
-        conf = new Configuration();
-        try {
-            hdfs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
-            return hdfs.getHomeDirectory().toString();
-        } catch (URISyntaxException ex) {
-            throw new IOException(ex);
+        for (FileStatus s : shell.lsr("/tmp")) {
+            System.out.println("> " + s.getPath());
         }
-//        return "please specify which meta you need";
+        return "lalala";
     }
 
     @RequestMapping("/{table}/meta")
